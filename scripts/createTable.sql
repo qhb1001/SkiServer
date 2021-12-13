f822e858a44befd21096e60f720de172f84d09e3
bo-niu <niubo0402@hotmail.com>
2021-12-12 21:45:59 -0800

    remove index

--- scripts/createTable.sql
+++ scripts/createTable.sql
@@ -27,10 +27,5 @@ CREATE TABLE LiftRides (
     dayId INTEGER,
     time INTEGER,
     vertical INTEGER,
-    PRIMARY KEY (id),
-    INDEX resortId_index (resortId),
-    INDEX skierId_index (skierId),
-    INDEX liftId_index (liftId),
-    INDEX seasonId_index (seasonId),
-    INDEX dayId_index (dayId)
+    PRIMARY KEY (id)
 );
